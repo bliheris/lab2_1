d8091d18d2ae8c5f0df0090003c99346aae19aba
Maciej Bołtromiuk <maciejboltromiuk@gmail.com>
2015-03-20 10:56:57 +0100

    Refectored BinarySearchTest to use HamcrestMatchers

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -1,48 +1,53 @@
 package edu.iis.mto.bsearch;
 
-import junit.framework.TestCase;
-import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 public class BinarySearchTest {
 
     @Test
     public void searchedElementIsInSequence() {
         SearchResult result = BinarySearch.search(2, new int[]{2});
-        assertTrue(result.isFound());
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(1));
     }
 
     @Test
     public void searchedElementIsNotInSequence() {
         SearchResult result = BinarySearch.search(3, new int[]{2});
-        assertFalse(result.isFound());
+        assertThat(result.isFound(), is(false));
+        assertThat(result.getPosition(), is(-1));
     }
 
     @Test
-         public void searchedElementIsAtTheBeginning() {
+    public void searchedElementIsAtTheBeginning() {
         SearchResult result = BinarySearch.search(2, new int[]{2,4,6});
-        assertTrue(result.isFound());
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(1));
     }
 
     @Test
     public void searchedElementIsAtTheEnd() {
         SearchResult result = BinarySearch.search(6, new int[]{2,4,6});
-        assertTrue(result.isFound());
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(3));
     }
 
     @Test
     public void searchedElementIsInTheMiddle() {
         SearchResult result = BinarySearch.search(6, new int[]{2,4,6,8,10});
-        assertTrue(result.isFound());
+        assertThat(result.isFound(), is(true));
+        assertThat(result.getPosition(), is(3));
     }
 
     @Test
     public void searchedElementIsNotInLongerSequence() {
         SearchResult result = BinarySearch.search(5, new int[]{2,4,6,8,10});
-        assertFalse(result.isFound());
+        assertThat(result.isFound(), is(false));
+        assertThat(result.getPosition(), is(-1));
     }
 }
